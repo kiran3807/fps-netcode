1e450b8df66ddfa61e49f791ff93c91dde2c5f47
Kiran Yallabandi <kiranyallabandi@gmail.com>
2024-05-23 09:02:17 +0530

    WIP: refactoring the views away from vector logic

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,63 +1,164 @@
-const PADDLE_LENGTH = 100;
-const PADDLE_WIDTH = 10;
+const SURFACE_NORMALS = {
+    TOP : [0,-1],
+    BOTTOM : [0,1],
+    LEFT : [1,0],
+    RIGHT : [-1,0]
+}
 
-class Game { 
+class CanvasView {
 
-    constructor(ctx, gameCanvasWidth, gameCanvasHeight) {
+    // PADDLE_LENGTH = 100;
+    // PADDLE_WIDTH = 10;
+
+    constructor(canvasSelector, canvasWidth, canvasHeight) {
+        this.canvasSelector = canvasSelector
+        this.canvasContext = canvasSelector.getContext("2d");
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        
+        this.canvasSelector.width = canvasWidth;
+        this.canvasSelector.height = canvasHeight;
+
         this.playerOnePaddleX = 0;
         this.playerTwoPaddleX = 0;
-        this.gameCanvasWidth = gameCanvasWidth;
-        this.gameCanvasHeight = gameCanvasHeight;
-        this.ctx = ctx;
+
+        this.playerOnePaddleView = new PaddleView(this.canvasContext, canvasWidth);
+
+        this.canvasContext.fillStyle = "rgb(0 0 0)";
+        this.canvasContext.fillRect(0, 0, this.canvasWidth, this.canvasHeight);        
     }
 
-    movePlayer1Paddle(curX) {
+    // _renderPaddle(paddleX, mouseX) {
 
-        this.ctx.fillStyle = "rgb(0 0 0)";
-        this.ctx.fillRect(this.playerOnePaddleX, 0, PADDLE_LENGTH, PADDLE_WIDTH);
+    //     this.canvasContext.fillStyle = "rgb(0 0 0)";
+    //     this.canvasContext.fillRect(
+    //         paddleX, 0, this.PADDLE_LENGTH, this.PADDLE_WIDTH
+    //     );
 
-        if(curX - (PADDLE_LENGTH/2) <= 0) {
-            this.ctx.fillStyle = "rgb(255 0 0)";
-            this.ctx.fillRect(0, 0, PADDLE_LENGTH, PADDLE_WIDTH);
-            this.playerOnePaddleX = 0;
-        } else if(curX + (PADDLE_LENGTH/2) >= this.gameCanvasWidth){
-            this.ctx.fillStyle = "rgb(255 0 0)";
-            this.ctx.fillRect(this.gameCanvasWidth-PADDLE_LENGTH, 0, PADDLE_LENGTH, PADDLE_WIDTH);
-            this.playerOnePaddleX = this.gameCanvasWidth-PADDLE_LENGTH;
+    //     if(mouseX - (this.PADDLE_LENGTH/2) <= 0) {
+    //         this.canvasContext.fillStyle = "rgb(255 0 0)";
+    //         this.canvasContext.fillRect(0, 0, this.PADDLE_LENGTH, this.PADDLE_WIDTH);
+    //         paddleX = 0;
+    //     } else if(mouseX + (this.PADDLE_LENGTH/2) >= this.canvasWidth){
+    //         this.canvasContext.fillStyle = "rgb(255 0 0)";
+    //         this.canvasContext.fillRect(
+    //             this.canvasWidth-this.PADDLE_LENGTH, 0, this.PADDLE_LENGTH, this.PADDLE_WIDTH
+    //         );
+    //         paddleX = this.canvasWidth-this.PADDLE_LENGTH;
+    //     } else {
+    //         this.canvasContext.fillStyle = "rgb(255 0 0)";
+    //         this.canvasContext.fillRect(
+    //             mouseX-(this.PADDLE_LENGTH/2), 0, this.PADDLE_LENGTH, this.PADDLE_WIDTH
+    //         );
+    //         paddleX = mouseX - (this.PADDLE_LENGTH/2);
+    //     }
+
+    //     return paddleX;
+    // }
+
+    renderPlayerOnePaddle(mouseX) {
+        this.playerOnePaddleX = this.playerOnePaddleView.renderPaddle(this.playerOnePaddleX, mouseX);
+    }
+
+    renderPlayerTwoPaddle() {
+
+    }
+}
+
+class PaddleView {
+
+    PADDLE_LENGTH = 100;
+    PADDLE_WIDTH = 10;
+    constructor(canvasContext, canvasWidth) {
+        this.canvasContext = canvasContext;
+        this.canvasWidth = canvasWidth;
+    }
+
+    renderPaddle(paddleX, mouseX) {
+
+        this.canvasContext.fillStyle = "rgb(0 0 0)";
+        this.canvasContext.fillRect(
+            paddleX, 0, this.PADDLE_LENGTH, this.PADDLE_WIDTH
+        );
+
+        if(mouseX - (this.PADDLE_LENGTH/2) <= 0) {
+            this.canvasContext.fillStyle = "rgb(255 0 0)";
+            this.canvasContext.fillRect(0, 0, this.PADDLE_LENGTH, this.PADDLE_WIDTH);
+            paddleX = 0;
+        } else if(mouseX + (this.PADDLE_LENGTH/2) >= this.canvasWidth){
+            this.canvasContext.fillStyle = "rgb(255 0 0)";
+            this.canvasContext.fillRect(
+                this.canvasWidth-this.PADDLE_LENGTH, 0, this.PADDLE_LENGTH, this.PADDLE_WIDTH
+            );
+            paddleX = this.canvasWidth-this.PADDLE_LENGTH;
         } else {
-            this.ctx.fillStyle = "rgb(255 0 0)";
-            this.ctx.fillRect(curX-(PADDLE_LENGTH/2), 0, PADDLE_LENGTH, PADDLE_WIDTH);
-            this.playerOnePaddleX = curX - (PADDLE_LENGTH/2);
+            this.canvasContext.fillStyle = "rgb(255 0 0)";
+            this.canvasContext.fillRect(
+                mouseX-(this.PADDLE_LENGTH/2), 0, this.PADDLE_LENGTH, this.PADDLE_WIDTH
+            );
+            paddleX = mouseX - (this.PADDLE_LENGTH/2);
         }
+
+        return paddleX;
+    }
+}
+
+class Ball {
+    
+    constructor(initX, initY, initialSpeed, initialUnitVector) {
+
+        this.currentX = initX;
+        this.currentY = initY;
+        this.speed = speed;
+        this.unitVector = unitVector;
+
+        window.requestAnimationFrame(()=> {
+
+        });
+    }
+
+    setBallPosition(time) {
+        this.currentX = this.currentX + this.unitVector[0]*time;
+        this.currentY = this.currentY + this.unitVector[1]*time;
+    }
+
+    setUnitVectorAfterCollision(surfaceNormal) {
+        
+        const constant = 
+        -2 * (this.unitVector[0]*surfaceNormal[0] + this.unitVector[1] * surfaceNormal[1]);
+
+        this.unitVector = [
+            this.unitVector[0]-(constant * surfaceNormal[0]), 
+            this.unitVector[1]-(constant * surfaceNormal[1])
+        ];
     }
 }
 
 window.addEventListener('load', function() {
 
     const canvas = document.querySelector("#game-canvas");
-    const ctx = canvas.getContext("2d");
+    // const ctx = canvas.getContext("2d");
 
-    const gameCanvasWidth = (canvas.width = window.innerWidth);
-    const gameCanvasHeight = (canvas.height = window.innerHeight);
+    // const gameCanvasWidth = (canvas.width = window.innerWidth);
+    // const gameCanvasHeight = (canvas.height = window.innerHeight);
 
-    ctx.fillStyle = "rgb(0 0 0)";
-    ctx.fillRect(0, 0, gameCanvasWidth, gameCanvasHeight);
-    const game = new Game(ctx, gameCanvasWidth, gameCanvasHeight);
+    // ctx.fillStyle = "rgb(0 0 0)";
+    // ctx.fillRect(0, 0, gameCanvasWidth, gameCanvasHeight);
+    //const game = new Game(ctx, gameCanvasWidth, gameCanvasHeight);
+
+    // document.addEventListener("mousemove", (e) => {
+    //     game.movePlayer1Paddle(e.pageX);   
+    // });
+
+
+    // ctx.fillStyle = "rgb(0 0 255)";
+    // ctx.beginPath();
+    // ctx.arc(150, 106, 50, 0, 2*Math.PI, false);
+    // ctx.fill();
+
+    const view = new CanvasView(canvas, window.innerWidth, window.innerHeight);
 
     document.addEventListener("mousemove", (e) => {
-        game.movePlayer1Paddle(e.pageX);   
+        view.renderPlayerOnePaddle(e.pageX);   
     });
-
-    // ctx.fillStyle = "rgb(0 255 0)";
-    // ctx.fillRect(75, 75, 100, 100);
-
-    // ctx.lineWidth = 5;
-    // ctx.strokeStyle = "rgb(255 255 255)";
-    // ctx.strokeRect(25, 25, 175, 200);
-
-    // ctx.fillStyle = "red";
-    // ctx.font = "48px georgia";
-    // ctx.fillText("Wilcommen", 50, 150);
-
-
 });
